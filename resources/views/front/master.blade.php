dfa41d3783f39c3be7dd17b28811820f44f57ef1
naglaamossleh2016 <naglaa.mossleh@gmail.com>
2024-05-18 17:41:23 +0300

    front

---
+++ resources/views/front/master.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@include('front.partials.head')
+
+<body>
+    <div class="container-xxl bg-white p-0">
+        <!-- Spinner Start -->
+        <div id="spinner"
+            class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+            <div class="spinner-grow text-primary" style="width: 3rem; height: 3rem;" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
+        <!-- Spinner End -->
+
+
+        <!-- Navbar & Hero Start -->
+        <div class="container-xxl position-relative p-0">
+            @include('front.partials.nav')
+            @yield('header-content')
+        </div>
+        <!-- Navbar & Hero End -->
+
+        @yield('content')
+
+        <!-- Footer Start -->
+        @include('front.partials.footer')
+        <!-- Footer End -->
+
+
+        <!-- Back to Top -->
+        <a href="#" class="btn btn-lg btn-secondary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+    </div>
+
+    @include('front.partials.scripts')
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,27 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+// require __DIR__.'/auth.php';
+Route::prefix('/')->name('front.')->group(function () {
+    Route::view('','front.index')->name('index'); //INDEX PAGE
+    Route::view('about','front.about')->name('about'); //About PAGE
+    Route::view('contact','front.contact')->name('contact'); //CONTACT PAGE
+    Route::view('services','front.services')->name('services'); //SERVICES PAGE
+    Route::view('projects','front.projects')->name('projects'); //PROJECTS PAGE
+    Route::view('team','front.team')->name('team'); //TEAM PAGE
+     Route::view('testimonials','front.testimonials')->name('testimonials'); //PROJECTS PAGE
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
